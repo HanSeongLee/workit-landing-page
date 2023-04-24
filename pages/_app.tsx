5d323112a5d7f6af2678111f161da30ac7ff0ae3
HanSeongLee <jkhanseong@gmail.com>
2023-04-24 18:05:37 +0900

    ADD | Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Fraunces, Manrope } from '@next/font/google';
+
+const manrope = Manrope({
+  weight: ['400'],
+  subsets: ['latin'],
+});
+
+const fraunces = Fraunces({
+    weight: ['600'],
+    subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+            :root {
+              --manrope-font: ${manrope.style.fontFamily};
+              --fraunces-font: ${fraunces.style.fontFamily};
+            }
+        `}</style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
