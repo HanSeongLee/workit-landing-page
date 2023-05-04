f626dffb6c7fb8f3973bd46b23390949a719d07d
HanSeongLee <jkhanseong@gmail.com>
2023-05-04 17:52:25 +0900

    ADD | Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,40 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Header from 'components/commons/Header';
+import HeroSection from 'components/HeroSection';
+import FeatureSection from 'components/FeatureSection';
+import Footer from 'components/commons/Footer';
+import CTASection from 'components/CTASection';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
 
-export default Home
+    const onApplyClick = () => {
+        alert('Apply button clicked!');
+    };
+
+    const handleLearnMoreClick = () => {
+        const featureSection = document.getElementById('feature-section');
+        if (!featureSection) {
+            return ;
+        }
+        const offset = 60;
+        window.scrollTo({
+            top: featureSection.offsetTop + offset,
+            behavior: 'smooth',
+        });
+    };
+
+    return (
+        <div className={styles.home}>
+            <Header onApplyClick={onApplyClick} />
+            <main>
+                <HeroSection onLearnMoreClick={handleLearnMoreClick} />
+                <FeatureSection id={'feature-section'} />
+                <CTASection onApplyClick={onApplyClick} />
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
