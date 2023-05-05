b8346593b9b676c51297d16429bcb5ea58a055ce
HanSeongLee <jkhanseong@gmail.com>
2023-05-05 18:47:35 +0900

    ADD | Storybook theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'dark',
+    brandTitle: 'workit',
+    brandImage: './logo-light.svg',
+    brandTarget: '_self',
+});
